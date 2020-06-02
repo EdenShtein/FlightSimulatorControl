b2963f6bcb80b762e0e0a7ea4351b2f12846be39
Eden Shtein <58863205+EdenShtein@users.noreply.github.com>
2020-06-02 14:14:25 +0300

    Delete CacheManager.java

--- server_side/CacheManager.java
+++
@@ -1,7 +0,0 @@
-package server_side;
-
-public interface CacheManager<Problem,Solution> {
-	public Boolean Check(Problem in);
-	public Solution Extract(Problem in);
-	public void Save(Problem in,Solution out);
-}
