625596d1bf85032ea69618eafc6ec9000d39c42c
Eden Shtein <58863205+EdenShtein@users.noreply.github.com>
2020-06-02 14:16:01 +0300

    Delete Searchable.java

--- server_side/Searchable.java
+++
@@ -1,9 +0,0 @@
-package server_side;
-
-import java.util.ArrayList;
-
-public interface Searchable {
-	State getInitialState();
-	State getGoalState();
-	ArrayList<State> getAllPossibleStates(State S);
-}
